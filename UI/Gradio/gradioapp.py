4ac93e2d1b8f7c099f4c072b09c7dc453cc3d989
TimurTKO <151398944+TimurTKO@users.noreply.github.com>
2024-07-29 22:19:54 +0300

    Added some files foe mlflow

--- UI/Gradio/gradioapp.py
+++ UI/Gradio/gradioapp.py
@@ -1,4 +1,17 @@
-import gradio_app as gr
+import gradio as gr
+import clickhouse_connect
+from dotenv import load_dotenv
+import os
+load_dotenv(".env")
+
+CLICKHOUSE_IP = os.getenv("CLICKHOUSE_IP")
+CLICKHOUSE_USER = os.getenv("CLICKHOUSE_USER")
+CLICKHOUSE_USER_PASSWORD = os.getenv("CLICKHOUSE_USER_PASSWORD")
+
+
+client = clickhouse_connect.get_client(host=CLICKHOUSE_IP,
+                                       username=CLICKHOUSE_USER,
+                                       password=CLICKHOUSE_USER_PASSWORD)
 
 def read_file(file):
     with open(file.name, 'r', encoding='utf-8') as f:
@@ -7,7 +20,7 @@ def read_file(file):
 
 iface = gr.Interface(
     fn=read_file,
-    inputs=gr.inputs.File(label="Upload a file"),
+    inputs=gr.File(label="Upload a file"),
     outputs="text",
     title="File Uploader",
     description="Upload a text file to display its content."
