7c6a50777e52cb8215fe8f96599d1482bb3fc147
TimurTKO <timurmadi@gmail.com>
2024-11-08 17:50:15 +0000

    addeed some changes to stream.py

--- API/streamlit/stream.py
+++ API/streamlit/stream.py
@@ -3,14 +3,11 @@ import pandas as pd
 import requests
 import os
 
-
 st.title("Предсказание на основе модели")
 st.write("Загрузите файл с данными для предсказания")
 st.write(os.environ.get('FASTAPI_URL'))
 
 uploaded_file = st.file_uploader("Выберите CSV файл", type="csv")
-
-#FASTAPI_URL = os.getenv('FASTAPI_URL', 'http://localhost:8000')  # Локальный URL по умолчанию на случай локального тестирования
 
 click = st.sidebar.button("Predict")
 if click:
@@ -21,32 +18,26 @@ if click:
             "json_str": json_str
         }
 
-
-        #url = "http://fastapi:8000/receivedataframe"
-         # URL эндпоинта FastAPI для обработки DataFrame
         FASTAPI_URL = os.getenv('FASTAPI_URL')
-        #url = f"{FASTAPI_URL}/receivedataframe"
-        # Checking if FASTAPI_URL is set
+        
+        # Проверка, установлен ли FASTAPI_URL
         if not FASTAPI_URL:
             st.error("URL сервиса FastAPI не найден.")
         else:
             url = f"{FASTAPI_URL}/receivedataframe"
             response = requests.post(url, json=payload)
+            
+            # Обработка ответа только если запрос успешен
+            if response.status_code == 200:
+                predictions = response.json()
+                df_predictions = pd.DataFrame(data={"predictions": predictions})
+                csv = df_predictions.to_csv(index=False)
 
-        #response = requests.post(FASTAPI_URL, json=payload)
-
-        predictions = response.json()
-
-        df_predictions = pd.DataFrame(data={"predictions": predictions})
-
-
-        csv = df.to_csv(index=False)
-
-        st.download_button(
-            label="Скачать результаты", 
-            data=csv,  # передаем строку CSV, а не DataFrame
-            file_name="results.csv", 
-            mime="text/csv"
-        )
-
-
+                st.download_button(
+                    label="Скачать результаты",
+                    data=csv,
+                    file_name="results.csv",
+                    mime="text/csv"
+                )
+            else:
+                st.error("Ошибка при запросе к сервису FastAPI. Проверьте URL и доступность сервиса.")
